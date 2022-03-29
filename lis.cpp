1b7bb4c2c2d202bd9aee4694a7aab7bf135d2a45
fahimmahmud618 <75488772+fahimmahmud618@users.noreply.github.com>
2022-03-29 17:11:38 +0600

    Update lis.cpp

--- lis.cpp
+++ lis.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 #define MAX 100
-int arr[MAX], length[MAX], sub_sequence[MAX];
+int arr[MAX], length[MAX], sub_sequence[MAX], reverse[MAX],t=0;
 
 bool max(int x, int y)
 {
@@ -9,6 +9,39 @@ bool max(int x, int y)
         return true;
     else
         return false;
+}
+
+int ret_maxpos(int size)
+{
+    int max = length[0];
+    int pos=0;
+    for(int i=0;i<size;i++)
+    {
+        if(max<length[i])
+        {
+            max=length[i];
+            pos=i;
+        }
+    }
+    cout<<"Maximum at the pos: "<<pos;
+    return pos;
+    
+}
+int backtrack(int pos)
+{
+    
+    reverse[t]=arr[pos];
+    if(length[pos]==-1)
+    {
+        cout<<reverse[t];
+        return t;
+    }
+    
+    cout<<reverse[t]<<" ";
+    pos = sub_sequence[pos];
+        t++;
+        backtrack(pos);
+    return t;
 }
 
 int main()
@@ -22,7 +55,7 @@ int main()
         scanf("%d",&arr[i]);
         length[i]=1;
     }
-
+    length[0]=-1;
     for(i=1;i<n;i++)
     {
         for(j=0;j<i;j++)
@@ -30,7 +63,7 @@ int main()
             if(arr[j]<arr[i])
             {
                 cout<<"hoise";
-                if(max(length[j]+1,length[i])) //modification needed
+                if(max(length[j]+1,length[i])) //modification
                 {
                     cout<<"__HA__";
                     length[i]=length[j]+1;
@@ -41,9 +74,12 @@ int main()
         }
     }
     
-    for(i=1;i<n;i++)
+    for(i=0;i<n;i++)
     {
-        printf("%d\t%d\t%d\n",arr[i],length[i],sub_sequence[i]);
+        printf("\n%d\t%d\t%d",arr[i],length[i],sub_sequence[i]);
     }
+    
+    cout<<"value of t : "<<backtrack(ret_maxpos(n));
+    
     return 0;
 }
